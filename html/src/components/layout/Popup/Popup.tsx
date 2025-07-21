1c1b608b169405878b6d1565687c95e92c40449d
minjung <togatecomz.mj@gmail.com>
2025-07-21 16:21:06 +0900

    feat: 팝업 콘텍스트

--- html/src/components/layout/Popup/Popup.tsx
+++ html/src/components/layout/Popup/Popup.tsx
@@ -1,18 +1,20 @@
+import { usePopup } from '../../../contexts/PopupContext';
 import styles from './Popup.module.scss';
-// import { usePopup } from "../../../contexts/PopupContext";
 
 const Popup = () => {
-  // const { isOpen, text, closePopup } = usePopup();
-  // if (!isOpen) return null;
+  const { isOpen, text, closePopup } = usePopup();
+  if (!isOpen) return null;
 
-  // const onBackgroundClick = () => closePopup();
-  // const onPopupClick = (e: React.MouseEvent) => e.stopPropagation();
+  const onBackgroundClick = () => closePopup();
+  const onPopupClick = (e: React.MouseEvent) => e.stopPropagation();
 
   return (
-    <div className={styles.popupWrapper}>
-      <div className={styles.popup}>
-        <div className={styles.text}></div>
-        <button className={styles.button}></button>
+    <div className={styles.popupWrapper} onClick={onBackgroundClick}>
+      <div className={styles.popup} onClick={onPopupClick}>
+        <div className={styles.text}>{text.message}</div>
+        <button className={styles.button} onClick={closePopup}>
+          {text.button}
+        </button>
       </div>
     </div>
   );
