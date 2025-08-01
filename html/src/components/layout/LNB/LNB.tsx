97909206ee39e6fc2d5df1c397756252f0988981
minjung <togatecomz.mj@gmail.com>
2025-08-01 14:15:30 +0900

    chore:LNB selected 로직 추가

--- html/src/components/layout/LNB/LNB.tsx
+++ html/src/components/layout/LNB/LNB.tsx
@@ -1,9 +1,12 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import styles from './LNB.module.scss';
 import Copyright from '../Copyright/Copyright.tsx';
 import MENU from '../../../constants/menu.ts';
 
 function LNB() {
+  // classname(selected) 확인용 로직
+  const location = useLocation();
+
   return (
     <aside className={styles.Lnb}>
       <nav className={styles.Menu}>
@@ -11,14 +14,19 @@ function LNB() {
           <section key={menu.name}>
             <h3 className={styles.MenuTitle}>{menu.name}</h3>
             <ul className={styles.SubMenu}>
-              {menu.children.map(child => (
-                <li key={child.name}>
-                  <Link to={child.path ? child.path : '#'} className={styles.SubMenuItem}>
-                    <i className={`${styles.Icon} ${styles[child.class]}`}></i>
-                    {child.name}
-                  </Link>
-                </li>
-              ))}
+              {menu.children.map(child => {
+                // classname(selected) 확인용 로직
+                const isSelected = location.pathname === child.path;
+
+                return (
+                  <li key={child.name}>
+                    <Link to={child.path ? child.path : '#'} className={`${styles.SubMenuItem} ${isSelected ? styles.selected : ''}`}>
+                      <i className={`${styles.Icon} ${styles[child.class]}`}></i>
+                      {child.name}
+                    </Link>
+                  </li>
+                );
+              })}
             </ul>
           </section>
         ))}
